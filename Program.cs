a0c35b3439d9deef8533ed035832ddb0d9407025
Gil <carlosgeovaniosilva@gmail.com>
2023-05-20 14:34:12 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DesignPatternsGOF.Singleton;
+
+Singleton object1 = Singleton.Instance();
+Singleton object2 = Singleton.Instance();
+
+if (object1 == object2)
+{
+    Console.WriteLine("These objects are the same");
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿/* 
+ * Why would we even use Singleton?
+ * Logging, drives, settings, etc. Often need only of a kind. 
+ * Maybe you want to update a setting and it be made globally.
+ *
+ * Reasons NOT to:
+ *  - Unit Testig
+ *	- Thread Safety
+ *	- Maybe a simples static class would be better?
+ *	- Violates SRP
+*/
+
+namespace DesignPatternsGOF.Singleton
+{
+    public class Singleton
+    {
+        protected Singleton() { }
+        static Singleton? instancce;
+        public string Setting { get; set; } = "Foo bar baz";
+
+        public static Singleton Instance()
+        {
+            if (instancce is null)
+            {
+                instancce = new Singleton();
+            }
+            return instancce;
+        }
+    }
+}
